d68a60554ef0c1f67df46f7dcc01f96c90f20811
Muhammed Niyas V P <mdniyas.vp@gmail.com>
2025-11-10 16:50:09 +0530

    Time: 551 ms (65.89%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -0,0 +1,8 @@
+
+ SELECT DATE_FORMAT(trans_date, '%Y-%m')AS month,
+ country,count(*) as trans_count,
+  count(case when state ='approved' then 1 end) as approved_count,
+  sum(amount)as trans_total_amount,
+   sum(case when state = 'approved'then amount else 0 end) as approved_total_amount
+FROM transactions
+group by DATE_FORMAT(trans_date, '%Y-%m'),country;
