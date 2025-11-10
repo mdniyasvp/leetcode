3b32326d17705affadb2a51aeb79afe7b88828de
Muhammed Niyas V P <mdniyas.vp@gmail.com>
2025-11-10 17:20:41 +0530

    Time: 580 ms (67.53%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -0,0 +1,7 @@
+select round(sum(case when order_date = customer_pref_delivery_date then 1 else 0 end)* 100/count(*),2) as immediate_percentage
+from delivery
+WHERE (customer_id, order_date) IN (
+    SELECT customer_id, MIN(order_date)
+    FROM Delivery
+    GROUP BY customer_id
+);
