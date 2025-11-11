9a45eb664ccbd2f84d5fe06c015f3d3e3070ce82
Muhammed Niyas V P <mdniyas.vp@gmail.com>
2025-11-11 13:42:29 +0530

    Time: 800 ms (50.73%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,9 @@
+
+select last_value(person_name) over(order by weight range between unbounded preceding and current row) as person_name from(
+    select sum(weight) over(order by turn asc) as total_weight,
+    person_name, weight,turn 
+    from queue
+) as t
+ where  total_weight <=1000
+ order by turn desc
+ limit 1;
